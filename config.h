a6c64222c8dcf7e733918aef54f686a777773f36
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-20 17:15:04 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
-static char *scriptfile     = "~/.surf/script.js";
-static char *styledir       = "~/.surf/styles/";
-static char *certdir        = "~/.surf/certificates/";
-static char *cachedir       = "~/.surf/cache/";
-static char *cookiefile     = "~/.surf/cookies.txt";
+static char *scriptfile     = "~/.config/surf/script.js";
+static char *styledir       = "~/.config/surf/styles/";
+static char *certdir        = "~/.config/surf/certificates/";
+static char *cachedir       = "~/.config/surf/cache/";
+static char *cookiefile     = "~/.config/surf/cookies.txt";
+static char *dlstatus       = "~/.config/surf/dlstatus/";
 static char *dldir          = "~/Downloads/";
-static char *dlstatus       = "~/.surf/dlstatus/";
 static char *searchurl      = "https://www.google.com/search?q=";
 
 /* Webkit default features */
